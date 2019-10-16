7816413437ad74f45a93e394163f43d3ae2f2f12
G. Liviu <ic01oblivionP@gmail.com>
2019-10-16 15:51:45 +0300

    edited object naming for user roles
    NUE to NUA (NUA => NovelUserAdmin)

--- app/Http/Middleware/CRUDAuthStaff.php
+++ app/Http/Middleware/CRUDAuthStaff.php
@@ -17,7 +17,8 @@ class CRUDAuthStaff
     public function handle($request, Closure $next)
     {
         if (Auth::user()) {
-            if (Auth::user()->isAuthorized('NUE', 'CRUD') || Auth::user()->isAuthorized('NU', 'CRUD')) {
+            // NUA = NovelUserAdmin || NU = NovelUser; CRUD = what actions can a user use
+            if (Auth::user()->isAuthorized('NUA', 'CRUD') || Auth::user()->isAuthorized('NU', 'CRUD')) {
                 return $next($request);
             } else {
                 return redirect('/')->with('success', 'You don\'t have permission to access this.');
