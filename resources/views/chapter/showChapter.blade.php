92d6f5725e01bb15a37977b56ee75f2d156ee974
G. Liviu <ic01oblivionP@gmail.com>
2019-10-16 16:43:56 +0300

    Improved UI experience
    - Added back-to-top button
    - Added navigation bar at the bottom of the page

--- resources/views/chapter/showChapter.blade.php
+++ resources/views/chapter/showChapter.blade.php
@@ -67,6 +67,42 @@
                     {{ $chapter->chapter_content }}
                 </p>
             </div>
+            
+            
+            <div class="row">
+                <div class="col-md-4 btnRow">
+                    @if($previousChapter)
+                        <a href="{{url("/novels/{$novel->slug}/chapter/{$previousChapter->slug}")}}"
+                           class="chapterBtn btn btn-outline-success">Previous</a>
+                    @endif
+                </div>
+
+                <div class="col-md-4 btnRow">
+                    <div class="btn-group chapterBtnO">
+                        <button type="button"
+                                class="btn btn-outline-success">{{ $chapter->chapter_name }}</button>
+                        <button type="button" class="btn btn-outline-success dropdown-toggle dropdown-toggle-split"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="sr-only">Toggle Dropdown</span>
+                        </button>
+                        <div class="dropdown-menu">
+                            @foreach($novel->chapters as $chapters)
+                                <a class="dropdown-item"
+                                   href="{{url("/novels/{$novel->slug}/chapter/{$chapters->slug}")}}">{{ $chapters->chapter_name }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 btnRow">
+                    @if($nextChapter)
+                        <a href="{{url("/novels/{$novel->slug}/chapter/{$nextChapter->slug}")}}"
+                           class="chapterBtn btn btn-outline-success">Next</a>
+                    @endif
+                </div>
+            </div>
+            <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button"
+               title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><i
+                    class="fas fa-arrow-up"></i></a>
         </div>
     </div>
 
